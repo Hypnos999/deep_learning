388bdec387989c13856802e4eea78702a746c6b3
Hypnos999 <flama2204@gmail.com>
2024-06-15 03:44:52 +0200

    commit

--- project/bert.py
+++ project/bert.py
@@ -1,13 +1,12 @@
+import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
-import tensorflow as tf
-from keras.layers import Dense, Dropout
-from keras.models import Model
-from keras.optimizers import Adam
+from keras.src.layers import Dense, Dropout, Input
+from keras.src.models import Model
+from keras.src.optimizers import Adam
+from keras_nlp.src.models import BertPreprocessor, BertBackbone
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
-# from official.nlp import optimization  # to create AdamW optimizer
 
 # BERT model selected           : https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1
 # Preprocess model auto-selected: https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3
@@ -16,8 +15,6 @@ from sklearn.model_selection import train_test_split
 if __name__ == '__main__':
     
     # Preprocessing
-    import os
-    print(os.getcwd())
     df = pd.read_csv('data/WELFake_Dataset.csv')
     df = df.dropna().drop_duplicates().sample(10000)
     
@@ -27,34 +24,40 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42, shuffle=True, stratify=y)
 
     # Carica il modello BERT base
-    bert_preprocess = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3', trainable=False)
-    bert_model = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3', trainable=False)
-    bert_model.trainable = False
-    bert_preprocess.trainable = False
-    output = bert_model(bert_preprocess('Ciao come stai?'))
-    print(output)
+    # bert_preprocess = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3', trainable=False)
+    # bert_model = hub.KerasLayer('https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3', trainable=False)
+    #
+    # text_input = Input(shape=(), dtype=tf.string, name='text')
+    # encoder_inputs = bert_preprocess(text_input)
+    # encoder_outputs = bert_model(encoder_inputs)
 
-    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-    encoder_inputs = bert_preprocess(text_input)
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string)
+    preprocessor = BertPreprocessor.from_preset("bert_base_en_uncased")
+    # preprocessor.trainable = False
+    encoder_inputs = preprocessor(text_input)
+    encoder = BertBackbone.from_preset("bert_base_en_uncased")
+    encoder.trainable = False
+    encoder_outputs = encoder(encoder_inputs)
+    # pooled_output = outputs["pooled_output"]  # [batch_size, 768].
+    # sequence_output = outputs["sequence_output"]  # [batch_size, seq_length, 768].
 
-    outputs = bert_model(encoder_inputs)
-    #x = outputs['pooled_output']
-    #x = Dense(128, activation='relu')(x)
-    #x = Dropout(0.3)(x)
-    #x = Dense(64, activation='relu')(x)
-    #x = Dropout(0.3)(x)
-    #x = Dense(2, activation='softmax')(x)
+    x = encoder_outputs['pooled_output']
+    # x = Dense(128, activation='relu')(x)
+    # x = Dropout(0.3)(x)
+    x = Dense(64, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(2, activation='softmax')(x)
     model = Model(text_input, x)
 
     # Costruisci il modello completo
     optimizer = Adam(learning_rate=5e-5)
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
-    metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
-    model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+    model.compile(optimizer=optimizer, loss='crossentropy', metrics=['accuracy'])
     print(model.summary())
     history = model.fit(x_train, y_train, epochs=5)
+    model.save('MyFakeBERT')
+    model.save_weights('MyFakeBERTWeights')
 
-    # loss, accuracy = model.evaluate(test_dataset)
-    # print(f'Test Loss: {loss:.4f}')
-    # print(f'Test Accuracy: {accuracy:.4f}')
+    loss, accuracy = model.evaluate(x_test, y_test)
+    print(f'Test Loss: {loss:.4f}')
+    print(f'Test Accuracy: {accuracy:.4f}')
 
